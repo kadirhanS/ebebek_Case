54971be556f6fad5b37189ec91679a69aaef88cb
kadirhanS <113786882+kadirhanS@users.noreply.github.com>
2022-10-13 03:34:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Employee  employee = new Employee("kadirhan sairiyilmaz",1000,42,2016);
+        System.out.println(employee.toString());
 
     }
 }
