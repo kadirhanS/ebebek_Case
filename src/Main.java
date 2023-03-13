8f99432bab959eaa030bb3ff0d44dca9accf6cb6
kadirhan <kdrhn2001@outlook.com>
2023-03-13 17:53:03 +0300

    main class'a yeni kodlar eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         Employee  employee = new Employee("kadirhan sairiyilmaz",1000,42,2016);
         System.out.println(employee.toString());
         System.out.println("kadirhan sariyilmaz");
+
+
+//        yeni branch de kodlar ekliyorum
+        System.out.println("1. kod ");
+        System.out.println("2. kod");
+        System.out.println("3. kod");
     }
 }
