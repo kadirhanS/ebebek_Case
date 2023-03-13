026c7aa34500a045b253af38cc25b287de4f5800
Kadirhan Sarıyılmaz <113786882+kadirhanS@users.noreply.github.com>
2023-03-13 17:48:34 +0300

    main class'da bazı kodlar cıkarıldı

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,5 @@ public class Main {
         Employee  employee = new Employee("kadirhan sairiyilmaz",1000,42,2016);
         System.out.println(employee.toString());
         System.out.println("kadirhan sariyilmaz");
-
-//        git komut denemeler
-        System.out.println("kendi branch'imde yeni commitler");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-//        birden fazla degisiklikler yapıldı yukarida
     }
 }
